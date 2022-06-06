49a0d48bd9fdcec8fce4a9c5d7db9f63c88e4d70
Anush Mutyala <69090449+anmutechsupport@users.noreply.github.com>
2022-06-06 19:43:35 -0400

    init

--- src/device.utils.ts
+++ src/device.utils.ts
@@ -1,50 +1,36 @@
-
 // -------------------------------------------------------
 // Synthetic Data Generation
 // -------------------------------------------------------
 
-
-let looping = false
-export const ondata = (decoded:string)=> {
-    let channelData = decoded.split(',').map(str => Number.parseFloat(str)) // Organize Decoder Output into a Float Array
-    return channelData // Pass Array to DataTracks
-}
-
+// let looping = false;
+export const ondata = (decoded: string) => {
+  let channelData = decoded.split(",").map((str) => Number.parseFloat(str)); // Organize Decoder Output into a Float Array
+  return channelData; // Pass Array to DataTracks
+};
 
 // -------------------------------------------------------
 // Template Callbacks
 // -------------------------------------------------------
 
-export const onconnect = async (device:any) => {
+// const cortexDevice = {
+//   // Generic
+//   label: "cortex",
+//   onconnect: () => {
+//     console.log("Cortex Connected");
+//   },
+//   protocols: [],
+// };
 
-    // Create synthetic data stream
-    let freqs = [1,5,10]
-    looping = true
-    let animate = () => {
+export const onconnect = () => {
+  console.log("Cortex Connected");
+};
 
-        if (looping){
-            let channels:number[] = []
-            freqs.forEach(f => {
-                channels.push(Math.sin((2 * f * Math.PI) * Date.now() / 1000))
-            })
-            let encoded = channels.join(',') // simulate encoding
-            device.ondata(encoded)
+export const ondisconnect = async (device: any) => {
+  //   looping = false;
+  console.log("Device disconnected", device);
+};
 
-            setTimeout(animate, 1000/60)
-        }
-
-    }
-
-    animate()
-    return true
-}
-
-export const ondisconnect = async (device:any) => {
-    looping = false
-    console.log('Device disconnected', device)
-}
-
-export const onerror = console.error
+export const onerror = console.error;
 
 // -------------------------------------------------------
 // Template Encoder / Decoder Specifications
@@ -52,10 +38,10 @@ export const onerror = console.error
 
 let encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
-export const encode = (msg:any, _:string) => {
-    return encoder.encode(msg)
-}
+export const encode = (msg: any, _: string) => {
+  return encoder.encode(msg);
+};
 
-export const decode = (msg:any, _:string) => {
-    return decoder.decode(msg)
-}
+export const decode = (msg: any, _: string) => {
+  return decoder.decode(msg);
+};
